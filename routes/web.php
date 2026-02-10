a2a76d2d2314f532c18d252b5be7e7ed5477aba5
Camilo Rojas <crojaslfc@gmail.com>
2026-02-10 14:58:19 -0500

    Bases de PHP

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+Route::get('/php-basico', function () {  
+    echo "<h1>Hola Mundo desde PHP</h1>";  
+    $nombre = "Camilo";   
+    $apellido = "Rojas";
+    $nombre_completo = $nombre . " " . $apellido;
+    $age = rand(10, 54);
+    $height = 1.85;
+    $isLogin = (bool) rand(0, 1);
+    echo "<h2>Mi nombre es: $nombre_completo</h2>";
 
-Route::get('dashboard', function () {
-    return Inertia::render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    echo "<h1> ************************ ESTRUCTURAS DE CONTROL ************************* </h1>";
 
-require __DIR__.'/settings.php';
+    $message = "Soy $nombre_completo, tengo $age años, mido $height metros y mi estado de login es: " . ($isLogin ? "Activo" : "Inactivo");
+    echo "<p>$message</p>";
+
+    if ($age >= 18) {
+        $message = "<p> Soy mayor de edad </p>";
+        echo $message;  
+    } else {
+        $message = "<p> Soy menor de edad </p>";
+        echo $message;
+    }
+    $message .= " ". ($isLogin ? "Estoy logueado" : "No estoy logueado");
+
+    echo "<h1> ************************ FUNCIONES EN PHP ************************* </h1>";
+
+    echo printUserInfo($nombre_completo, $age);
+
+    $productoNames = ["Laptop", "Smartphone", "Tablet", "Monitor", "Teclado", 25, true, "Cámara", "Auriculares", "Altavoces"];
+    $teclado = [
+    "nombre" => "Teclado Mecánico",
+    "precio" => 99.99,
+    "stock" => 50,
+    'distribuciones' => ["Amazon", "eBay", "MercadoLibre"],
+    ];
+
+    $teclado["marca"] = "Logitech";
+    echo $teclado["nombre"];
+
+    foreach ($productoNames as $item)
+        echo "<p>Producto: $item</p>";
+});
+
+function printUserInfo(string $name, int $age) {
+    return "El nombre del usuario es $name y tiene $age años.";
+}
